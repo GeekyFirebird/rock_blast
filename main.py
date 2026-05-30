1b0192183a5b2c270b53458e6c26a92f7ac80296
geekyfirebird <geekyfirebird@gmail.com>
2026-05-30 18:00:02 -0400

    Set up player and fix window flashes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from logger import log_state
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_RADIUS
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -9,21 +10,23 @@ def main():
 
     pygame.init()
 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Sets the screen size
+
     # Restrict our game to draw a maximum of 60 times per second, or 60 FPS
     clock = pygame.time.Clock()
     dt = 0.0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
 
     while True:
-        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Sets the screen size
         log_state() # 
         for event in pygame.event.get():
             if event.type == pygame.QUIT: # Makes the windows close button work. 
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip() #Refresh the screen
         dt = clock.tick(60) / 1000 # limit the framerate to 60 FPS
-
         # Restrict our game to draw a maximum of 60 times per second, or 60 FPS
 
 
